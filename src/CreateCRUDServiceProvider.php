18d78b57cb137525854b923ad2246eea700388a5
Javier Manga <javier.manga@iagt.es>
2019-12-23 16:08:19 +0100

    Upload

--- src/CreateCRUDServiceProvider.php
+++ src/CreateCRUDServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace JaviManga\InitDB;
+namespace JaviManga\CreateCrud;
 
+use JaviManga\CreateCrud\Console\Commands\CreateCRUD;
 use JaviManga\InitDB\Console\Commands\CloneDB;
 use JaviManga\InitDB\Console\Commands\CreateDB;
 use JaviManga\InitDB\Console\Commands\DropDB;
-use JaviManga\InitDB\Console\Commands\createCRUD;
 use Illuminate\Support\ServiceProvider;
 
 class CreateCRUDServiceProvider extends ServiceProvider
